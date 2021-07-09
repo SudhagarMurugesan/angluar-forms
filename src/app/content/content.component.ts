4d7abb7bba18a3373424211edaa532ff10b2f577
sudhagarmsd5 <54501573+sudhagarmsd5@users.noreply.github.com>
2021-07-09 19:33:46 +0530

    commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,30 +8,41 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class ContentComponent implements OnInit {
 
   requiredForm: FormGroup;
+
+ 
+    emailFormControl= new FormControl('', [Validators.required, Validators.email]);
+
+    PostalForm=new FormControl('', [
+      Validators.required, Validators.pattern("^[0-9]*$")
+    ]);
+
+    phoneFormControl= new FormControl('', [
+      Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"),
+      Validators.maxLength(10)
+    ]
+    )
+
+
   constructor(private fb: FormBuilder) {
-     this.myForm();
+    this.myForm();
   }
 
   myForm() {
     this.requiredForm = this.fb.group({
-    name: ['', Validators.required ],
-    address: ['', Validators.required ],
-    address1: ['', Validators.required ],
-    city:['', Validators.required ],
-    state:['', Validators.required ],
-    country:['', Validators.required ],
-    postal:['', Validators.required ],
-    phone:['', Validators.required ],
-    email:['', Validators.required,Validators.email ]
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      address1: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+      postal: ['', Validators.required, Validators.pattern("^[0-9]*$")],
+      phone: ['', Validators.required],
+      email: ['', Validators.required, Validators.email]
     });
- }
-
-  ngOnInit() {
-    
   }
 
-  public enableSubmitBtn(): Boolean{
-    return this.requiredForm.valid;
+  ngOnInit() {
+
   }
 
 }
